513abbf1189c427e1fd63b3f2b987523fa3779e4
josechavez-bit <77467474@idexperujapon.edu.pe>
2024-10-02 20:49:56 -0500

    paginaweb

---
+++ pagina.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EN LINEA</title>
+    <link rel="stylesheet" href="resources\resources\css\bootstrap.min.css">
+    <link rel="stylesheet" href="resources/resources/css/bootstrap-icons/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="./resources/resources/css/main.css">
+</head>
+
+<body class="px-3">
+    <div class="row mt-3">
+        <div class="col-sm-12 col-md-6 mt-4">
+            <h1 class="text-center"><i class="bi bi-slash-square text-red rounded"></i> EN LINEA</h1>
+        </div>
+        <div class="col-sm-12 col-md-6 p-4 pr-5">
+            <p class="text-right">SOLUCIONES MUNDIALES</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <img class="w-100 card-img-top" style="height: 350px;" src="./resources/resources/img/baner01.PNG" alt="">
+            <div class="card-img-overlay">
+                <h4 class="card-title m-3">
+                    <span class="bg-vine text-white">¿Necesitas<br>ayuda? ¡Nunca había sido<br>tan fácil ser reconocido!</span></h4>
+                <p class="card-text m-3">
+                    <span class="bg-dark text-white">Permítenos demostrarte lo que nuestro equipo<br>puede hacer por ti y tu negocio.</span></p>
+            </div>
+        </div>
+    </div>
+<div class="container mt-4">
+    <h1 class="h1">SOLUCIONES CREATIVAS</h1>
+    <P>sin importar cual se tu negocio, nuestro 
+        equipo puede hacerlo desde cero.</P>
+        <div class="row">
+            <div class="col-sm-12 col-md-4">
+                <div class="card border-0">
+                <div class="card-body">
+                    <img class="w-100" src="./resources/resources/img/idea_01.PNG" alt="">
+                    <h5 class="h5 mt-2">
+                        <i class="bi bi-pencil-fill bg-dark text-white rounded"></i> DISEÑO</h5>
+                    <p>Creamos diseños según tus necesidades</p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <div class="card border-0">
+                <div class="card-body">
+                    <img class="w-100" src="./resources/resources/img/idea_02.PNG" alt="">
+                    <h5 class="h5 mt-2">
+                        <i class="bi bi-flower3 bg-dark text-white rounded"></i> DESARROLLO</h5>
+                    <p>Nuestros programadores te garantizan la perfección</p>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <div class="card border-0">
+                <div class="card-body">
+                    <img class="w-100" src="./resources/resources/img/idea_03.PNG" alt="">
+                    <h5 class="h5 mt-2">
+                    <i class="bi bi-arrows-move bg-dark text-white rounded"></i> EDICIÓN DE LIBROS</h5>
+                    <p>Que el producto final llegue a tus clientes</p>
+                </div>
+                </div>
+            </div>
+        </div>
+</div>
+<footer class="bg-vine text-white text-center p-2">
+    Edificio Las hadas, Guadalajara wwww.sitioincribles.mx (33)32146547
+</footer>
+</body>
+</html>
